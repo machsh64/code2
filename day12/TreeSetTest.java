9156e677734bb477a116f5d7a34b0fba8820fc8f
machsh <3256772458@qq.com>
2022-09-27 20:14:43 +0800

    Set > HashSet LinkedHashSet TreeSet

---
+++ day12/TreeSetTest.java
@@ -0,0 +1,82 @@
+package day12;
+
+import org.junit.Test;
+
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.TreeSet;
+
+/**
+ * @program: code2
+ * @author: Ren
+ * @create: 2022-09-27 19:02
+ * @description:
+ **/
+public class TreeSetTest {
+    /**
+     * 1，向TreeSet中添加的数据，要求是相同类的对象。
+     * 2，两种排序方式：自然排序（实现Comparable接口）  和  定制排序（Comparator）
+     *
+     * 3，自然排序中，比较两个对象是否相同的标准为：compareTo()返回0，不再是equals()。
+     *
+     */
+
+    @Test
+    public void test1() {
+       TreeSet<Object> set = new TreeSet<>();
+
+       //失败：不能添加不同类的对象
+     /*  set.add(123);
+       set.add(456);
+       set.add("AA");
+       set.add(new User("Ton",12));*/
+
+        //举例一：
+       /* set.add(123);
+        set.add(456);
+        set.add(-12);
+        set.add(41);*/
+
+        //举例二：
+        set.add(new User("Tom",12));
+        set.add(new User("Jerry",32));
+        set.add(new User("Jim",2));
+        set.add(new User("Mike",65));
+        set.add(new User("Jack",32));
+        set.add(new User("Jack",56));
+
+        Iterator iterator = set.iterator();
+        while(iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+    }
+
+    @Test
+    public void test2() {
+        Comparator<Object> com = new Comparator() {
+            //按照年龄从小到大排列
+            @Override
+            public int compare(Object o1, Object o2) {
+                if (o1 instanceof User && o2 instanceof User){
+                    User u1 = (User) o1;
+                    User u2 = (User) o2;
+                    return Integer.compare(u1.getAge(),u2.getAge());
+                }
+                throw new RuntimeException();
+            }
+        };
+       TreeSet<Object> set = new TreeSet<>(com);
+        set.add(new User("Tom",12));
+        set.add(new User("Jerry",32));
+        set.add(new User("Jim",2));
+        set.add(new User("Mike",65));
+        set.add(new User("Jack",32));
+        set.add(new User("Mary",32));
+        set.add(new User("Jack",56));
+
+        Iterator iterator = set.iterator();
+        while(iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+    }
+}
--- day12/User.java
+++ day12/User.java
@@ -8,7 +8,7 @@ import java.util.Objects;
  * @create: 2022-09-27 17:59
  * @description:
  **/
-public class User {
+public class User implements Comparable {
     private String name;
     private int age;
 
@@ -54,5 +54,22 @@ public class User {
     @Override
     public int hashCode() {
         return Objects.hash(name, age);
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        if (o instanceof User) {
+            User u = (User) o;
+            //按年龄从小到大排
+            /*return Double.compare(this.age,u.age);*/
+            //按姓名从小到大排
+            int compare = this.name.compareTo(u.name);
+            if (compare != 0) {
+                return compare;
+            } else if (compare == 0) {
+                return Double.compare(this.age,u.age);
+            }
+        }
+        throw new RuntimeException();
     }
 }
