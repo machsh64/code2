5a760815c8870c603b1c74ed3d406dfc26740fef
machsh <3256772458@qq.com>
2022-10-02 12:58:09 +0800

    通配符

--- day15/GenericTest.java
+++ day15/GenericTest.java
@@ -112,4 +112,64 @@ public class GenericTest {
         System.out.println(list);
     }
 
+    //通配符的使用
+    @Test
+    public void test5() {
+        List<String> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+
+        List<?> list = null;
+        list = list1;
+        list = list2;
+        //编译通过
+//        print(list1);
+//        print(list2);
+
+
+        List<String> list3 = new ArrayList<>();
+        list3.add("AA");
+        list3.add("BB");
+        list3.add("CC");
+        list = list3;
+        //添加（写入）：对于List<?>就不能向其内部添加数据
+        //除了添加null之外
+//        list.add("AA");
+        list.add(null);
+
+        //允许读取数据，读取的数据类型为Object
+        Object o = list.get(0);
+        System.out.println(o);  //AA
+        print(list);  //AA BB CC null
+    }
+
+    public void print(List<?> list){
+        Iterator<?> iterator = list.iterator();
+        while(iterator.hasNext()){
+            Object obj = iterator.next();
+            System.out.println(obj);
+        }
+    }
+
+    //有限制条件的通配符的使用
+      /**
+      *   ? extends A:  G<? extends A>  可以作为G<A> 和 G<B> 的子类 其中A是B的父类
+       *  ? super B:    G<? super A>  可以作为G<A> 和 G<B> 的父类 其中B是A的父类
+       */
+      @Test
+      public void test6() {
+          List<? extends Person> list1 = null;
+          List<? super Person> list2 = null;
+
+          List<Student> list3 = new ArrayList<>();
+          List<Person> list4 = new ArrayList<>();
+          List<Object> list5 = new ArrayList<>();
+
+          list1 = list3;
+          list1 = list4;
+        //  list1 = list5;   报错，要求list1 必须是继承或等于Person的类
+
+        //  list2 = list3;   报错，要求list2 必须是Person的父类或者Person
+          list2 = list4;
+          list2 = list5;
+      }
 }
---
+++ day15/Student.java
@@ -0,0 +1,10 @@
+package day15;
+
+/**
+ * @program: code2
+ * @author: Ren
+ * @create: 2022-10-02 12:18
+ * @description:
+ **/
+public class Student extends Person{
+}
